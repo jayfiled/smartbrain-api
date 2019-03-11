3d56fb0f5fcd51ae2ceec15779c6a74229651d91
jayfiled <jayfiled@gmail.com>
2019-03-11 23:04:21 +1100

    set up server for back end smart brain

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('This is working! ')
+})
+
+app.listen(3000, () => {
+    console.log('App is running on port 3000')
+});
+
+
+// API Plan
+
+/* 
+
+/ --> res = this is working
+/signin 
+
+
+
+*/
