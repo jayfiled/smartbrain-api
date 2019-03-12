82797dafd14e6400a6f7bbb600807f3bc9cb9b7c
jayfiled <jayfiled@gmail.com>
2019-03-12 12:13:42 +1100

    map out API design

--- server.js
+++ server.js
@@ -15,9 +15,20 @@ app.listen(3000, () => {
 
 /* 
 
-/ --> res = this is working
-/signin 
+legend:
 
+endpoint --> via this type of request = respond with
+
+/ --> res = this is working
+/signin --> POST = success / fail
+/register --> POST = user object
+/profile/:userId --> GET = user
+
+// We want to work with a ranking; when someone submits a new photo
+// their ranking will go up, and a variable as a counter that goes up,
+// and checks against other users score to give them a rank.
+
+/image --> PUT = user
 
 
 */
